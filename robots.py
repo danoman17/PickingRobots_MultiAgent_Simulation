2afcd2562207d6a0ba7f59b7b8ad6cb497f68009
danoman17 <daniflores374@gmail.com>
2021-11-30 15:01:03 -0600

    added functionalities

--- robots.py
+++ robots.py
@@ -1,168 +1,350 @@
-import random
-import time
-
 from mesa import Agent, Model
 from mesa.space import MultiGrid
 from mesa.time import RandomActivation
 from mesa.visualization.modules import CanvasGrid
-from mesa.visualization.modules import TextElement
 from mesa.visualization.ModularVisualization import ModularServer
-from mesa.visualization.UserParam import UserSettableParameter
+
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid as PathGrid
+from pathfinding.finder.a_star import AStarFinder
 
 
 class Robot(Agent):
-    def __init__(self, model, pos, stackBoxes):
+    def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
-        self.pos = pos
-        self.stackBoxes = stackBoxes
+        
+        self.pos = pos # robot coordenates
+        self.numberBoxesStack = 0 # no. of boxes that are been carried by the robot       
+        self.selfMovents = 0 # no. of movements that this robot is making
+        self.pickedBoxes = 0 # no. of picked boxes since the start of the simulation individualy
+        self.auxIndex = 0 # Auxilar index to handle the positions traversed in our path to an empty closest stack tile. 
+        self.allocatedStack = 0 # Assigned Stack tile where the robot most get the picked boxes
+        self.tileType = "Robot"
+        self.empty = True # attribute that help to leave boxes in a stack tile
+        self.leavedBoxes = False # attribute for APIs purposes
+
+
 
     def step(self):
+
         # Usamos los vecinos de la posición actual considerando las diagonales
         next_moves = self.model.grid.get_neighborhood(self.pos, moore=True)
         # Seleccionamos alguno de los vecinos de forma aleatoria y nos movemos a él
         next_move = self.random.choice(next_moves)
 
-        # Se guarda la lista de los agentes que se encuentran en la posición a la que se quiere dirigir el robot
-        # Esta lista puede contener un agente DirtyTile y/o 1 o más agentes Robots
-        angentsList = self.model.grid.get_cell_list_contents(next_move)
 
-        # Iteramos por cada agente
-        for agent in angentsList:
-            # Si dicho agente es una celda sucia, la limpiamos removiendo el dicho agente
-            if(str(type(agent).__name__) == "Box"):
-                self.model.grid.remove_agent(agent)
-                
-                # También disminuimos el total de celdas con basura en el grid
-                self.model.trashAmount -= 1
-
-                self.stackBoxes += 1;
-                break
+        # checamos si está lleno el stack del robot actual
+        print("en stack del robot actual: ", self.numberBoxesStack)
+        if self.numberBoxesStack == 5:
+            isfullStack = True
+        else:
+            isfullStack = False
         
-        if self.stackBoxes >= 5:
-            print("terminó un robot, debes de dejar las cajas")
-            self.stackBoxes = 0
 
-        # Movemos el robot a su nueva posición
-        self.model.grid.move_agent(self, next_move)
+        if not isfullStack:
+            print("no estoy lleno")
+            self.leavedBoxes = False 
+            isClean = self.pickAction()
+            print("clean: ", isClean)
+            
+            if self.selfMovents >= self.model.time:
+                print("Time's over, total movents: ", self.selfMovents * self.model.robots)
+            if(self.model.boxNo == 0 and self.isAllRobotsEmpty()):
+                print("All boxes have been picked up, adding up to: ", self.getTotalMovements(), " of total movents")
+            if( self.selfMovents < self.model.time and isClean ):
+                self.selfMovents += 1
+                self.model.grid.move_agent(self, next_move)
+        else:
+            print("estoy lleno")
 
-        # Aumentamos el total de movimientos de los robots
-        self.model.totalMovimientos += 1
+            pathToStack = self.getPathToClosestTileStack(self.allocatedStack)
+            print("path: ",pathToStack, "len: ", len(pathToStack), "auxIndex: ", self.auxIndex)
+            if( self.auxIndex < len(pathToStack) ):
+                next_move = pathToStack[self.auxIndex]
+                self.auxIndex = 1
+                self.model.grid.move_agent(self, next_move)
+            else:
+                self.auxIndex = 0
+                self.numberBoxesStack = 0
+                print("dejo cajas xd")
+                self.leaveBoxes()
+                self.leavedBoxes = True
+                self.assignNewStack()
+            
+            isClean = True
+
+
+
+    def pickAction(self):
+        listTiles = self.model.grid.get_cell_list_contents([self.pos])
+
+        for tile in listTiles:
+
+            if self.model.boxNo == 0:
+                if self.numberBoxesStack == 0:
+                    self.empty = True
+                    return False
+                elif self.numberBoxesStack != 0:
+                    self.createArrayOfNonFullStack()
+                    self.goToEmptyTileStack()
+
+            if (tile.tileType == "Box"):
+
+                tile.clean = True
+                tile.assignStackPosX = self.allocatedStack[0]
+                tile.assignStackPosY = self.allocatedStack[1]
+                tile.changeColor()
+                self.pickedBoxes += 1
+                self.numberBoxesStack += 1
+                tile.inStack = self.numberBoxesStack
+                print("Bloques recogidos: ", self.numberBoxesStack)
+                self.model.boxNo -= 1
+                print("bloques restantes: ", self.model.boxNo)
+                return False
+            else:
+                return True
+    
+    def getTotalMovements(self):
+        movs = 0
+        for robot in self.model.robsArray:
+            movs += robot.selfMovents
+        return movs
+
+    def isAllRobotsEmpty(self):
+        for robot in self.model.robsArray:
+            if robot.empty == False:
+                return False
+        return True
+    
+
+    def leaveBoxes(self):
+        listTiles = self.model.grid.get_cell_list_contents([self.pos])
+        for tile in listTiles:
+            if( tile.tileType == "Stack"):
+                tile.boxesInTile = 5
+                self.numberBoxesStack = 0
+                self.auxIndex = 0
+
+    def assignNewStack(self):
+        if( len( self.model.stacksPos ) > 0 ):
+            stackIndex = self.random.randint(0, len( self.model.stacksPos ) - 1)
+            tupleOfStack = self.model.stacksPos[stackIndex]
+
+            self.allocatedStack = tupleOfStack
+            del self.model.stacksPos[stackIndex]
+
+    
+     #funcion auxiliar que saca la ruta hacia el stack mas cercano
+    def getPathToClosestTileStack(self, path):
+      grid = PathGrid(matrix=self.model.matrix)
+      #print(self.model.matrix)
+      grid.cleanup()
+      start = grid.node(self.pos[0],self.pos[1])
+      print("start x:", self.pos[0]," y: ", self.pos[1])
+      end = grid.node(self.allocatedStack[0],self.allocatedStack[1])
+      print("end x:", self.allocatedStack[0]," y: ", self.allocatedStack[1])
+      finder = AStarFinder(diagonal_movement=DiagonalMovement.never)
+      path, runs = finder.find_path(start, end, grid)
+      return path
         
+
+    def getPathToEmptyTileStack(self, stack):
+
+        print(self.model.matrix)
+        grid.cleanup()
+        
+        start = grid.node( self.pos[0], self.pos[1] )
+        end = grid.node( stack.pos[0], stack.pos[1] )
+        finder = AStarFinder( diagonal_movement=DiagonalMovement.never )
+        path, runs = finder.find_path(start, end, grid)
+        return path
+
+    def createArrayOfNonFullStack(self):
+        indexAux = 0
+        for stack in self.model.arrStacks:
+            if stack.full:
+                del self.model.arrStacks[indexAux]
+            indexAux += 1
+
+        for stack in self.model.arrStacks:
+            print("Stacks without been filled yet: ", stack.boxes, " pos: ", stack.pos)
+
+    def goToEmptyTileStack(self):
+        if ( len(self.model.arrStacks) > 0 ):
+            path = self.getPathToEmptyTileStack(self.model.arrStacks[0])
+            if( self.auxIndex < len( path ) ):
+                next_move = path[self.auxIndex]
+                self.auxIndex += 1
+                self.model.grid.move_agent(self, next_move)
+            else:
+                self.auxIndex = 0
+                self.leaveMissingBoxes()
+    
+    def leaveMissingBoxes(self):
+        listTiles = self.model.grid.get_cell_list_contents([self.pos])
+
+        for tile in listTiles:
+            if( tile.tileType == "Stack" ) :
+                if( tile.boxes < 5 ):
+                    if( ( tile.boxes + self.numberBoxesStack ) < 5 ):
+                        tile.boxes = tile.boxes + self.numberBoxesStack
+                        self.empty = True
+                        self.numberBoxesStack = 0
+                    elif ( ( tile.boxes + self.numberBoxesStack ) == 5  ):
+                        tile.boxes = 5
+                        self.numberBoxesStack = 0
+                        tile.full = True
+                    else:
+                        spareBoxes = ( tile.boxes + self.numberBoxesStack ) - 5
+                        tile.boxes = 5
+                        tile.full = True
+                        self.numberBoxesStack = spareBoxes
+                else:
+                    print("shouldn't get here")
 
 # Usamos esta clase para crear las celdas sucias (las identificamos como agentes sin movimiento) 
 class Box(Agent):
-     def __init__(self, model, pos):
+    def __init__(self, model, pos):
+        super().__init__(model.next_id(), model)
+        self.tileType = "Box"
+        self.pos = pos
+        self.assignStackPosX = -1
+        self.assignStackPosY = -1
+        self.inStack = -1
+        self.clean = False
+
+    def changeColor(self):
+        self.tileType = "stdTile"
+
+
+class stdTile(Agent):
+    def __init__(self, model, pos):
         super().__init__(model.next_id(), model)
         self.pos = pos
+        self.clean = True
+        self.tileType = "stdTile"
+
+
+class Stack(Agent):
+    def __init__(self, model, pos):
+        super().__init__(model.next_id(), model)
+        self.pos = pos
+        self.boxes = 0
+        self.full = False
+        self.tileType = "Stack"
+        
 
 class Floor(Model):
-    # Declaramos estos como atributos de la clase para poder usarlos en las demás clases que 
-    # tengan acceso al modelo
-    totalTime = 0
-    percentageClean = 0
-    trashAmount = 0
-    totalMovimientos = 0
-
-    def __init__(self, x=50, y=50, numRobots=10, trashPercentage=50, maxTime=60):
+    #se asignan las variables modificables por el usuario siendo filas, columnas, robots, tiempo de ejecucion y el numero de bloques sucios
+    def __init__(self, rows =10,columns = 10, robotsNo = 1, time = 20000, boxNo = 60):
         super().__init__()
+
         self.schedule = RandomActivation(self)
 
-        self.x = x
-        self.y = y
+        self.rows = rows
+        self.columns = columns
+        self.robotsNo = robotsNo
+        self.time = time
+        self.boxNo = boxNo
+        self.Stacks = (boxNo // 5)
+  
 
-        # Usamos un MultiGrid para poder colocar múltiples agentes en una misma celda
-        self.grid = MultiGrid(x, y, torus=False)
+        self.grid = MultiGrid(self.columns, self.rows, torus=False)
 
-        # Declaramos nuestras variables que ingresará el usuario desde el input
-        self.startTime = 0
-        self.maxTime = maxTime
-
-        # Cuando el counter haya aumentado (es decir, después de la primera llamada al constructor)
-        # se pueden pedir los datos de porcentaje de basura, número de Robots y duración máxima
-        self.startTime = time.time()
-        
-        # Calculamos el total de basura que se debe insertar en la matriz 
-        trashTotal = int((x*y) * (trashPercentage/100))
-
-        # Lo declaramos como global y vacío por si en otras llamadas al constructor se habían incrementado sus elementos 
-        self.trashAmount = 0
-
-        # Reiniciamos el conteo de movimientos en cada llamada al constructor
-        self.totalMovimientos = 0
-
-        # Creamos una lista con números aleatorios en el rango de la basura que desea el usuario
-        randomNumsList = random.sample(range(x*y), trashTotal)
-
-        # Insertamos la basura iterativamente en posiciones aleatorias
-        for i in randomNumsList:
-          posY = i//x
-          posX = i%x
-          # llamamos a la clase DirtyTile y la instanciamos con las coordenadas de la matriz declarada.
-          boxTile = Box(self, (posX, posY))
-          # Aumentamos la cantidad de basura en el grid
-          self.trashAmount += 1
-          # enviamos como parametro a la clase place_agent para que se muestre en el navegador
-          self.grid.place_agent(boxTile, boxTile.pos)
-          
-        #Inicializacion de clase Robot para la entrada de n modelos
-        for _ in range(numRobots):
-          gh = Robot(self, (1, 1))
-          self.grid.place_agent(gh, gh.pos)
-          self.schedule.add(gh)
+        self.matrix = []
+        self.stacksPos = []
+        self.arrStacks = []
+        self.robsArray = []
+        #se crea una matriz de ceros para identificar las casillas sucias y limpias
+        self.createZeroMatrix()
+        #se crean los robots y bloques tanto limpios como sucios para colocarse en el tablero
+        self.setStacks()
+        self.setRobot()
+        self.setBox()
+        self.setStdTile()
 
     def step(self):
         self.schedule.step()
+    
+    @staticmethod
+    def count_type(model):
+        return model.boxNo
+    
+    def createZeroMatrix(self):
+        for i in range(0, self.rows):
+            zeros = []
+            for j in range(0, self.columns):
+                zeros.append(0)
+            self.matrix.append(zeros)
 
-        # Si el tiempo actual sobrepasa el tiempo en el que debe terminar la simulación, se detiene la simulación
-        if(self.startTime + self.maxTime < time.time() or self.trashAmount == 0):
-            self.running = False
+    def setBox(self):
+        tiles = self.boxNo
+        while tiles > 0:
+            randomPosX = self.random.randint(0, self.rows - 1)
+            randomPosY = self.random.randint(0, self.columns - 1)
+            while self.matrix[randomPosX][randomPosY] == 1:
+                randomPosX = self.random.randint(0, self.rows - 1)
+                randomPosY = self.random.randint(0, self.columns - 1)
+            tile = Box( self, (randomPosY, randomPosX) )
+            self.grid.place_agent( tile, tile.pos )
+            self.matrix[randomPosX][randomPosY] = 1
+            tiles -= 1
+            self.schedule.add(tile)
+    
+    def setStdTile(self):
+        for _, x, y in self.grid.coord_iter():
+            if self.matrix[y][x] == 0:
+                tile = stdTile( self, (x,y) )
+                self.grid.place_agent(tile, tile.pos)
+    
+    def setRobot(self):
+        for _ in range(0, self.robotsNo):
+            rob = Robot(self,(1,1))
+
+            if( len(self.stacksPos) > 0 ):
+                stackIndex = self.random.randint( 0, len(self.stacksPos) - 1)
+                stackTuple = self.stacksPos[stackIndex]
+                rob.allocatedStack = stackTuple
+                del self.stacksPos[stackIndex]
+
+            self.robsArray.append(rob)
+            self.grid.place_agent(rob, rob.pos)
+            self.schedule.add(rob)
+
+
+
+    def setStacks(self):
+        for _ in range(0,self.Stacks):
+            randomPosX = self.random.randint(0, (self.rows) -1)
+            randomPosY = self.random.randint(0, (self.columns) -1)
+
+            while self.matrix[randomPosX][randomPosY] == 1:
+                randomPosX = self.random.randint(0, (self.rows) -1)
+                randomPosY = self.random.randint(0, (self.columns) -1)
             
-            # Calculamos el tiempo que duró la simulación
-            self.totalTime = round(time.time() - self.startTime)
-
-            # Calculamos el porcentage de celdas limpias en el grid
-            totalCeldas = self.x*self.y
-            self.percentageClean = round(((totalCeldas-self.trashAmount)/totalCeldas) * 100, 2)
-
-# creamos la clase de TextResults, la cual nos devuelve una serie de etiquetas de texto con los resultados obtenidos durante y al final de la simulación. 
-# haciendo uso de HTML podemos colocar las etiquetas en nuestra aplicación con ayuda de etiquetas como <br> y <hr> para tener una mejor organización. 
-class TextResults(TextElement):
-    def render(self, model):
-        return f"""
-            <br>Tiempo necesario hasta que todas las celdas estén limpias (o se haya llegado al tiempo máximo): <b>{model.totalTime} segundos </b>
-            <hr>
-            Porcentaje de celdas limpias después del termino de la simulación: <b>{model.percentageClean}% </b>
-            <hr>
-            Número de movimientos realizados por todos los agentes: <b>{model.totalMovimientos} movimientos</b>
-        """
-
+            stack = Stack(self, ( randomPosY, randomPosX ))
+            self.grid.place_agent( stack, stack.pos )
+            self.matrix[randomPosX][randomPosY] = 1
+            
+            self.stacksPos.append(stack.pos)
+            self.arrStacks.append(stack)
+            self.schedule.add(stack)
+    
 def agent_portrayal(agent):
-    # si el tipo de clase recibido es DirtyTile, retornamos un muro
-    if str(type(agent).__name__) == "DirtyTile":
-         return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "Gray", "Layer": 0}
-    # sino retornamos una imagen
-    return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "Blue", "Layer": 0}
+    if( agent.tileType == "Robot" ):
+        return {"Shape": "walle.png", "Layer": 0}
+    elif( agent.tileType == "Box" ):
+        return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "brown", "Layer": 1}
+    elif( agent.tileType == "stdTile"):
+        return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "#ced4da", "Layer": 1}
+    elif( agent.tileType == "Stack" ):
+        if agent.boxes == 5:
+            agent.full = True
+            return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "green", "Layer": 1}
+        return {"Shape": "rect", "w": 1, "h": 1, "Filled": "true", "Color": "red", "Layer": 1}
+    
+grid = CanvasGrid( agent_portrayal, 10, 10, 450, 450 )
 
-# Establecemos el tamaño del CanvasGrid que sea de 50*50 celdas
-grid = CanvasGrid(agent_portrayal, 50, 50, 500, 500)
-
-# Hacemos uso de la funcion de UserSettableParameter, la cual nos da una serie de widgets que nos permiten tener una mejor interacción con la simulación a traves de sliders, inputs de texto, entre otros, lo que nos permite manipuilar facilmente los valores iniciales.
-server = ModularServer(Floor, [grid, TextResults()], "PacMan", {
-  
-    # Leemos el dato y con ayuda de un Slider
-    "y": UserSettableParameter('slider', 'Número de filas', value=50, min_value=2, max_value=50, step=1),
-
-    # Leemos el dato x con ayuda de un Slider
-    "x": UserSettableParameter('slider', 'Número de columnas', value=50, min_value=2, max_value=50, step=1),
-
-    # A traves de un drop down box para ingresar el total de robots.
-    "numRobots": UserSettableParameter("number", "Número de robots", value=10), 
-
-    # A traves de un drop down box para ingresar el porcentaje de basura.
-    "trashPercentage": UserSettableParameter("number", "Porcentaje de basura (Formato: 50 -> 50%)", value=50),
-
-    # A traves de un drop down box para ingresar el la duracion máxima.
-    "maxTime": UserSettableParameter("number", "Duración máxima de la simulación (segundos)", value=60),
-})
-server.port = 8573
+server = ModularServer( Floor, [grid], "Store", {} )
+server.port = 8524
 server.launch()
